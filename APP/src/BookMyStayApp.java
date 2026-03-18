7213557c4018b32aafe3589eb8be77723ac2e494
Harrish2331 <harrishgoutham10@gmail.com>
2026-03-18 09:30:10 +0530

    Welcome page

---
+++ APP/src/BookMyStayApp.java
@@ -0,0 +1,17 @@
+public class BookMyStayApp{
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("===================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("===================================");
+
+        // Application ends here
+    }
+}
